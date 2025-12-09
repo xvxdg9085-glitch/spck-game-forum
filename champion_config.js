a4691d6b18feb3c641cf28066bb94c2c648db616
MinhKhoiM10 <xvxdg9085@gmail.com>
2025-12-09 20:26:22 +0700

    upd 1.1

---
+++ champion_config.js
@@ -0,0 +1,44 @@
+//https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/befd42ad6d2564159a441d08cfc3bf511532eb74-1628x1628.png?auto=format&fit=fill&q=80&w=870
+
+const akali = {
+  name: "Akali",
+  title: "The Rogue Assassin",
+  image: "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/befd42ad6d2564159a441d08cfc3bf511532eb74-1628x1628.png?auto=format&fit=fill&q=80&w=870",
+}
+const yasuo = {
+  name: "Yasuo",
+  title: "The Unforgiven",
+  image: "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/70c26e49de8a2c79ac3de144772d2debd195edff-1628x1628.png?auto=format&fit=fill&q=80&w=870",
+}
+const lux = {
+  name: "Lux",
+  title: "The Lady of Luminosity",
+  image: "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/ff6c8c57411e5c7e0551b02334fccedc78866143-1628x1628.png?auto=format&fit=fill&q=80&w=870",
+}
+const jinx = {
+  name: "Jinx",
+  title: "The loose cannon",
+  image: "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/f136500bd46f823d37515a72b867425d3a0b3e54-1628x1628.png?auto=format&fit=fill&q=80&w=635",
+}
+const thresh = {
+  name: "Thresh",
+  title: "The Chain Warden",
+  image: "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/dbdded937cd214bb2a1189697a9e4f49f8c04505-1628x1628.png?auto=format&fit=fill&q=80&w=870",
+}
+const leona = {
+  name: "Leona",
+  title: "The Radiant Dawn",
+  image: "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/95daf6dd2b28f03d5ba2ea1fabbabc3bc3ff6e6e-1628x1628.png?auto=format&fit=fill&q=80&w=870",
+}
+function akaliactivate() {
+}
+function yasuoactivate() {
+}
+function luxactivate() {
+}
+function jinxactivate() {
+}
+function threshactivate() {
+}
+function leonaactivate() {
+}
