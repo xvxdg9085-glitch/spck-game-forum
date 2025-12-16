2479f9f2ee013583e98884a2ce06fa762af939c4
tlmk123 <tranleminhkhoi123laptrinh@gmail.com>
2025-12-16 20:13:20 +0700

    upd 1.2

--- champion_config.js
+++ champion_config.js
@@ -1,5 +1,9 @@
 //https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/befd42ad6d2564159a441d08cfc3bf511532eb74-1628x1628.png?auto=format&fit=fill&q=80&w=870
 
+const championimage = document.getElementById("champion-image");
+const championname = document.getElementById("champion-name");
+const championtitle = document.getElementById("champion-title");
+
 const akali = {
   name: "Akali",
   title: "The Rogue Assassin",
@@ -31,14 +35,61 @@ const leona = {
   image: "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/95daf6dd2b28f03d5ba2ea1fabbabc3bc3ff6e6e-1628x1628.png?auto=format&fit=fill&q=80&w=870",
 }
 function akaliactivate() {
+  championimage.src = akali.image;
+  championname.textContent = akali.name;
+  championtitle.textContent = akali.title;
+
 }
 function yasuoactivate() {
+  championimage.src = yasuo.image;
+  championname.textContent = yasuo.name;
+  championtitle.textContent = yasuo.title;
 }
 function luxactivate() {
+  championimage.src = lux.image;
+  championname.textContent = lux.name;
+  championtitle.textContent = lux.title;
 }
 function jinxactivate() {
+  championimage.src = jinx.image;
+  championname.textContent = jinx.name;
+  championtitle.textContent = jinx.title;
 }
 function threshactivate() {
+  championimage.src = thresh.image;
+  championname.textContent = thresh.name;
+  championtitle.textContent = thresh.title;
 }
 function leonaactivate() {
+  championimage.src = leona.image;
+  championname.textContent = leona.name;
+  championtitle.textContent = leona.title;
+}
+
+
+
+//Multiple ways to play part
+
+const rift = {
+  video_id: "https://cmsassets.rgpub.io/sanity/files/dsfx7636/news/bbc27473157462adacf0de441a8796268eb2d0ac.mp4?accountingTag=league_of_legends_website",
+  title: "The most popular game mode",
+  sub_title: "Clear your lane, dive into epic 5v5 team fights, and destroy the enemy nexus before they destroy yours."
+}
+const aram = {
+  video_id: "https://cmsassets.rgpub.io/sanity/files/dsfx7636/news/0a9b9f8dacb54086c58c1af8aa880d7cf6d7fea6.mp4?accountingTag=league_of_legends_website",
+  title: "No mercy, pure chaos!",
+  sub_title: "Battle across an icy bridge as your team of random champions charge toward the enemy Nexus in this chaotically fun 5v5 game mode."
+}
+const tft = {
+  video_id: "https://cmsassets.rgpub.io/sanity/files/dsfx7636/news/e5791dbc0787a96e83b82df20d44375f09f4d861.mp4?accountingTag=league_of_legends_website",
+  title: "Teamfight Tactics",
+  sub_title: "Choose your own strategy and outwit seven of your opponents and become the last person standing in this strategic game mode."
+}
+function rift_active() {
+
+}
+function aram_active() {
+
+}
+function tft_active() {
 }
